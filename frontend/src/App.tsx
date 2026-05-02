3c09f1df03dcb6a34bb94cff239028e28e481d96
dineshbabu99 <dineshkaru28@gmail.com>
2026-05-02 14:35:25 +0530

    fix: add netlify redirects

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,18 @@
 
-import { Navigate, Routes } from 'react-router'
-import { Route } from 'react-router-dom'
+import { Navigate, Routes, Route } from 'react-router-dom'
 import {Layout} from './components/Layout.tsx'
 import './App.css'
 import { Expense } from './components/Expense.tsx'
 
 function App() {
-
   return (
-    <>
     <Routes>
-  <Route path="/" element={<Layout />}>
-    <Route index element={<Navigate to="/expenses" />} />
-    <Route path="expenses" element={<Expense />} />
-  </Route>
-</Routes>
-    </>
-  )
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Navigate to="/expenses" />} />
+        <Route path="expenses" element={<Expense />} />
+      </Route>
+    </Routes>
+  );
 }
 
 export default App
